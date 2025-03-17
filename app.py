1959fe2bc5bc92d71a3754325ac620e4310ff78d
Gzq <ziqi.guo@ucdconnect.ie>
2025-03-17 00:34:12 -0400

    cache

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from blueprints import register_blueprints
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024
 register_blueprints(app)
 
 @app.route('/')
