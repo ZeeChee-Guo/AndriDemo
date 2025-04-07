c9bcb3b5362b801d7a45660539c70644509dac7d
G-Jaqen <jaqen0413@gmail.com>
2025-04-07 12:01:24 -0400

    update

--- norma_cache/joblib/utils/ml/compute_global_scores/func_code.py
+++ norma_cache/joblib/utils/ml/compute_global_scores/func_code.py
@@ -1,17 +1,21 @@
-# first line: 16
+# first line: 20
 @memory.cache
 def compute_global_scores(data_arr):
     """Fit NORMA and return pattern_length and normalized scores with padding."""
-    pattern_length = find_length(data_arr)
-    clf_global = NORMA(
-        pattern_length=pattern_length,
-        nm_size=3 * pattern_length,
-        percentage_sel=1,
-        normalize='z-norm'
-    )
-    clf_global.fit(data_arr)
-    global_scores = np.array(clf_global.decision_scores_)
-    global_scores = MinMaxScaler(feature_range=(0, 1)).fit_transform(global_scores.reshape(-1, 1)).ravel()
-    pad = (pattern_length - 1) // 2
-    padded_scores = np.array([global_scores[0]] * pad + list(global_scores) + [global_scores[-1]] * pad)
+
+    f = io.StringIO()
+    with contextlib.redirect_stdout(f):
+        pattern_length = find_length(data_arr)
+
+        clf_global = NORMA(
+            pattern_length=pattern_length,
+            nm_size=3 * pattern_length,
+            percentage_sel=1,
+            normalize='z-norm'
+        )
+        clf_global.fit(data_arr)
+        global_scores = np.array(clf_global.decision_scores_)
+        global_scores = MinMaxScaler(feature_range=(0, 1)).fit_transform(global_scores.reshape(-1, 1)).ravel()
+        pad = (pattern_length - 1) // 2
+        padded_scores = np.array([global_scores[0]] * pad + list(global_scores) + [global_scores[-1]] * pad)
     return pattern_length, padded_scores
--- utils/ml.py
+++ utils/ml.py
@@ -9,26 +9,35 @@ from external.a2d2.util.util_a2d2 import find_length
 
 import warnings
 import pandas as pd
+import io
+import contextlib
 
 
 memory = Memory(location="./norma_cache", verbose=0)
 
+
+
 @memory.cache
 def compute_global_scores(data_arr):
     """Fit NORMA and return pattern_length and normalized scores with padding."""
-    pattern_length = find_length(data_arr)
-    clf_global = NORMA(
-        pattern_length=pattern_length,
-        nm_size=3 * pattern_length,
-        percentage_sel=1,
-        normalize='z-norm'
-    )
-    clf_global.fit(data_arr)
-    global_scores = np.array(clf_global.decision_scores_)
-    global_scores = MinMaxScaler(feature_range=(0, 1)).fit_transform(global_scores.reshape(-1, 1)).ravel()
-    pad = (pattern_length - 1) // 2
-    padded_scores = np.array([global_scores[0]] * pad + list(global_scores) + [global_scores[-1]] * pad)
+
+    f = io.StringIO()
+    with contextlib.redirect_stdout(f):
+        pattern_length = find_length(data_arr)
+
+        clf_global = NORMA(
+            pattern_length=pattern_length,
+            nm_size=3 * pattern_length,
+            percentage_sel=1,
+            normalize='z-norm'
+        )
+        clf_global.fit(data_arr)
+        global_scores = np.array(clf_global.decision_scores_)
+        global_scores = MinMaxScaler(feature_range=(0, 1)).fit_transform(global_scores.reshape(-1, 1)).ravel()
+        pad = (pattern_length - 1) // 2
+        padded_scores = np.array([global_scores[0]] * pad + list(global_scores) + [global_scores[-1]] * pad)
     return pattern_length, padded_scores
+
 
 
 def norm_a_scoring(data, flags, areas, z_threshold=-2.5, delta_threshold=0.07, clustering_diff_threshold=0.1):
