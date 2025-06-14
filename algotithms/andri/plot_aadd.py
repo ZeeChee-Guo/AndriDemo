acd699958998671f1d647096b828839d4e085612
G-Jaqen <jaqen0413@gmail.com>
2025-06-14 04:04:19 -0400

    add some example files

--- algotithms/andri/plot_aadd.py
+++ algotithms/andri/plot_aadd.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from external.a2d2.util.TSB_AD.metrics import metricor
+from algotithms.andri.metrics import metricor
 import sys
 
 # Return the intervals where there is an anomaly
